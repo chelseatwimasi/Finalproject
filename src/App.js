46d6e51c0c8b87951f4d8bec99b177f74334cb92
chelseatwimasi <89853022+chelseatwimasi@users.noreply.github.com>
2022-04-04 11:50:11 -0400

    Day2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
-import Search from "./Components/search";
+import CardList from "./Components/CardList";
+//import Search from "./Components/search";
+
+let data = [
+  "https://images.medicinenet.com/images/slideshow/heart-disease-visual-guide-s2-heart-attack.jpg"
+];
 
 class App extends Component {
   constructor(props) {
@@ -13,6 +18,7 @@ class App extends Component {
       <div className="h1">
         <h1>The New York Times</h1>
         <h2>Top Five Death Causing Diseases</h2>
+        <CardList images={data} />
       </div>
     );
   }
---
+++ src/Components/Card.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Card(props) {
+  return (
+    <div className="Example">
+      <h3>Hello example component!</h3>
+    </div>
+  );
+}
+
+export default Card;
