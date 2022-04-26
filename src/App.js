388613e7c573ecd95c1fd9368518ab9a8668f1eb
chelseatwimasi <89853022+chelseatwimasi@users.noreply.github.com>
2022-04-26 12:25:24 -0400

    Day7

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Card from "./Components/Card.js";
 import data from "./diseases.js";
 
-function App() {
-  return (
-    <div>
-      <div className="App">
-        <h1>The New York Times</h1>
-        <h2>Top Five Death Causng Diseases</h2>
-      </div>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-      <div className="container">
-        <ul className="cards">
-          {data.diseases.map((disease) => {
-            return <Card disease={disease} />;
-          })}
-        </ul>
+  render() {
+    return (
+      <div>
+        <div className="App">
+          <h1>The New York Times</h1>
+          <h2>Top Five Death Causng Diseases</h2>
+        </div>
+
+        <div className="container">
+          <ul className="cards">
+            {data.diseases.map((disease) => {
+              return <Card disease={disease} />;
+            })}
+          </ul>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 //function App() {
 // console.log(data.islands[0].name);
